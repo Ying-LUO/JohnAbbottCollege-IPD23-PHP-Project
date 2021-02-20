833c6dee6b35432937eb4001026e925f66295d04
Ying LUO <ying.luo@johnabbottcollege.net>
2021-02-20 18:39:23 -0500

    minor change

--- skirentals/account.php
+++ skirentals/account.php
@@ -11,7 +11,8 @@ use Slim\Views\Twig;
 require_once 'vendor/autoload.php';
 require_once 'init.php';
 
-$passwordPepper = 'mmyb7oSAeXG9DTz2uFqu';
+// generated random string
+$passwordPepper = 'vG3iNzWMwKARpChq5KDZ';
 
 // root page
 $app->get('/', function ($request, $response, $args) {
@@ -50,9 +51,6 @@ $app->post('/register', function ($request, $response, $args) use ($log) {
     if (strlen($userName) < 2 || strlen($userName) > 30) {
         $errorList['userName'] = "User Name must be 2-30 characters long";
         $userName = '';
-    }elseif (isUserNameTaken($userName)) {
-        $errors['userName'] = "User Name is already exist.";
-        $userName = '';
     }
     if(!preg_match("/^[0-9]{3}-[0-9]{3}-[0-9]{4}$/", $phone)) {
         $errorList['phone'] = "Phone: " . $phone . " must be like ***-***-****";
@@ -61,9 +59,6 @@ $app->post('/register', function ($request, $response, $args) use ($log) {
     if (filter_var($email, FILTER_VALIDATE_EMAIL) == false) {
         $errors['email'] = "Invalid Email";
         $email = '';
-    } elseif (isEmailTaken($email)) {
-        $errors['email'] = "Email is already exist.";
-        $email = '';
     }
     if (strlen($street) < 2 || strlen($street) > 100) {
         $errorList['street'] = "Street must be 2-100 characters long";
@@ -71,8 +66,8 @@ $app->post('/register', function ($request, $response, $args) use ($log) {
     if (strlen($city) < 2 || strlen($city) > 100) {
         $errorList['city'] = "City must be 2-100 characters long";
     }
-    if (!isset($province)) {
-        $errorList['province'] = "Province cannot be empty";
+    if (strlen($province) < 2 || strlen($province) > 30) {
+        $errorList['province'] = "Province must be 2-30 characters long";
     }
     if(!preg_match("/^[A-Za-z0-9]{3} [A-Za-z0-9]{3}$/", $postCode)) {
         $errorList['postalCode'] = "PostalCode: " . $postCode . " must be in XXX YYY format";
@@ -131,45 +126,30 @@ $app->post('/register', function ($request, $response, $args) use ($log) {
     }
 });
 
-$app->get('/register/isemailtaken/{email}', function ($request, $response, array $args) use ($log) {
-    $error = '';
-
-    if(isset($args['email'])){
-        $error = isEmailTaken($args['email']) ? "It's already taken." :'';
+// used via AJAX
+$app->get('/register/isemailtaken/{email}', function ($request, $response, $args) use ($log) {
+    // get email address from url
+    $email = isset($args['email']) ? $args['email'] : "";
+    $record = DB::queryFirstRow("SELECT * FROM users WHERE email=%s", $email);
+    if ($record) {
+        $log->debug(sprintf("Internal Error: duplicate email %s, uid=%d", $email, $_SERVER['REMOTE_ADDR']));
+        return $response->write("Email already in use");
+    } else {
+        return $response->write("");
     }
-
-    $response->getBody()->write($error);
-    return $response;
 });
 
-// used for register and validation
-function isEmailTaken($email)
-{
-    $users = DB::queryFirstRow("SELECT COUNT(*) AS 'count' FROM users WHERE email = %s", $email);
-
-    if ($users['count'] == 0) {
-        return false;
-    } elseif ($users['count'] == 1) {
-        return true;
+$app->get('/register/isusernametaken/{username}', function ($request, $response, $args) use ($log) {
+    // get username from url
+    $username = isset($args['username']) ? $args['username'] : "";
+    $record = DB::queryFirstRow("SELECT * FROM users WHERE email=%s", $username);
+    if ($record) {
+        $log->debug(sprintf("Internal Error: duplicate username %s, uid=%d", $username, $_SERVER['REMOTE_ADDR']));
+        return $response->write("UserName already in use");
     } else {
-        $log->debug(sprintf("Internal Error: duplicate email %s, uid=%d", $email, $_SERVER['REMOTE_ADDR']));
-        return true;
+        return $response->write("");
     }
-}
-
-function isUserNameTaken($userName)
-{
-    $users = DB::queryFirstRow("SELECT COUNT(*) AS 'count' FROM users WHERE username = %s", $userName);
-
-    if ($users['count'] == 0) {
-        return false;
-    } elseif ($users['count'] == 1) {
-        return true;
-    } else {
-        $log->debug(sprintf("Internal Error: duplicate User Name %s, uid=%d", $userName, $_SERVER['REMOTE_ADDR']));
-        return true;
-    }
-}
+});
 
 function verifyPasswordQuality($password) {
     if (strlen($password) < 6 || strlen($password) > 100
@@ -203,12 +183,7 @@ $app->post('/login', function ($request, $response, $args) use ($log) {
         $pwdHashed = $record['password'];
         if (password_verify($pwdPeppered, $pwdHashed)) {
             $loginSuccess = true;
-        }
-        // WARNING: only temporary solution to allow for old plain-text passwords to continue to work
-        // Plain text passwords comparison
-        else if ($record['password'] == $password) {
-            $loginSuccess = true;
-        }
+        } 
     }
     //
     if (!$loginSuccess) {
@@ -218,8 +193,8 @@ $app->post('/login', function ($request, $response, $args) use ($log) {
         unset($record['password']); // for security reasons remove password from session
         $_SESSION['user'] = $record; // remember user logged in
         $log->debug(sprintf("Login successful for email %s, uid=%d, from %s", $email, $record['id'], $_SERVER['REMOTE_ADDR']));
-        return $this->view->render($response, 'login_success.html.twig', ['userSession' => $_SESSION['user'] ] );
-        echo('userSession');
+        echo "<script>$('.alert').alert('Login Successfully');</script>";
+        return $this->view->render($response, 'index.html.twig', ['userSession' => $_SESSION['user'] ] );
     }
 });
 
@@ -244,8 +219,113 @@ $app->get('/account', function ($request, $response, $args) use ($log){
     }
 });
 
-$app->post('/account', function ($request, $response, $args) {
-    // update account information
+$app->post('/account', function ($request, $response, $args) use ($log) {
+    if(isset($_SESSION['user'])) {
+        $originUser = DB::queryFirstRow("SELECT * FROM users WHERE id=%d", $_SESSION['user']['id']);
+    }
+    if(isset($originUser)){
+
+        $firstName = $request->getParam('firstName');
+        $lastName = $request->getParam('lastName');
+        $userName = $request->getParam('userName');
+        $email = $request->getParam('email');
+        $phone = $request->getParam('phone');
+        $pass1 = $request->getParam('pass1');
+        $pass2 = $request->getParam('pass2');
+        $city = $request->getParam('city');
+        $street = $request->getParam('street');
+        $province = $request->getParam('province');
+        $postCode = $request->getParam('postCode');
+
+        $errorList = [];
+        if (strlen($firstName) < 2 || strlen($firstName) > 50) {
+            $errorList['firstName'] = "First Name must be 2-50 characters long";
+            $firstName = '';
+        }
+        if (strlen($lastName) < 2 || strlen($lastName) > 50) {
+            $errorList['lastName'] = "Last Name must be 2-50 characters long";
+            $lastName = '';
+        }
+        if (strlen($userName) < 2 || strlen($userName) > 30) {
+            $errorList['userName'] = "User Name must be 2-30 characters long";
+            $userName = '';
+        }
+        if(!preg_match("/^[0-9]{3}-[0-9]{3}-[0-9]{4}$/", $phone)) {
+            $errorList['phone'] = "Phone: " . $phone . " must be like ***-***-****";
+            $phone = '';
+        }
+        if (filter_var($email, FILTER_VALIDATE_EMAIL) == false) {
+            $errors['email'] = "Invalid Email";
+            $email = '';
+        }
+        if (strlen($street) < 2 || strlen($street) > 100) {
+            $errorList['street'] = "Street must be 2-100 characters long";
+        }
+        if (strlen($city) < 2 || strlen($city) > 100) {
+            $errorList['city'] = "City must be 2-100 characters long";
+        }
+        if (strlen($province) < 2 || strlen($province) > 30) {
+            $errorList['province'] = "Province must be 2-30 characters long";
+        }
+        if(!preg_match("/^[A-Za-z0-9]{3} [A-Za-z0-9]{3}$/", $postCode)) {
+            $errorList['postalCode'] = "PostalCode: " . $postCode . " must be in XXX YYY format";
+        }
+
+        $pass1Quality = verifyPasswordQuality($pass1);
+        $pass2Quality = verifyPasswordQuality($pass2);
+        if ($pass1Quality !== TRUE) {
+            $errorList['password1'] = $pass1Quality;
+        } elseif ( $pass2Quality !== TRUE) {
+            $errorList['password2'] = $pass2Quality;
+        }elseif ($pass1 !== $pass2) {
+            $errorList['password'] = "Passwords must be same.";
+        }
+
+        if ($errorList) {
+            $log->error(sprintf("Register failed: email %s, username %s, uid=%d", $email, $userName, $_SERVER['REMOTE_ADDR']));
+            return $this->view->render($response, 'account.html.twig', [
+                'errors' => $errorList,
+                'prevInput' => [
+                    'firstName' => $firstName,
+                    'lastName' => $lastName,
+                    'userName' => $userName,
+                    'phone' => $phone,
+                    'email' => $email,
+                    'pass1' => $pass1,
+                    'pass2' => $pass2,
+                    'street' => $street,
+                    'city' => $city,
+                    'province' => $province,
+                    'postalCode' => strtoupper($postCode)
+                ]
+            ]);
+        } else {
+            global $passwordPepper;
+            $pwdPeppered = hash_hmac("sha256", $pass1, $passwordPepper);
+            $pwdHashed = password_hash($pwdPeppered, PASSWORD_DEFAULT); // PASSWORD_ARGON2ID);
+
+            $updateUser = ['firstName' => $firstName,
+                'lastName' => $lastName,
+                'userName' => $userName,
+                'email' => $email,
+                'password' => $pwdHashed,
+                'phone' => $phone,
+                'street' => $street,
+                'city' => $city,
+                'province' => $province,
+                'postalCode' => strtoupper($postCode)];
+
+            DB::update('users', $updateUser, "id=%d", $originUser['id']);
+            // refresh new user data
+            $_SESSION['user'] = DB::queryFirstRow("SELECT * FROM users WHERE id = %d",$originUser['id']);
+            $log->debug(sprintf("Update user account successfully: new email %s, new username %s, uid=%d", $_SESSION['user']['email'], $_SESSION['username'], $_SERVER['REMOTE_ADDR']));
+            return $response->withHeader('Location', '/');
+        }
+
+    }else{
+        $log->error(sprintf("Internal Error: Cannot find userName %s\n:%s", $_SESSION['user']['username'], $_SERVER['REMOTE_ADDR']));
+        return $response->withHeader("Location", "/error_internal",403);
+    }
 });
 
 $app->get('/contact', function ($request, $response, $args) use ($log){
